aad93dc5044f3764d83573b3dcbf603f451e5997
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-04-27 00:23:10 -0400

    product details is goo

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
     return (
-        <div className='col-3'>
-            <div className='product-card'>
+        <Link className='col-3'>
+            <div className='product-card position-relative'>
+                <div className="wishlist-icon position-absolute">
+                    <Link>
+                        <img src="images/wish.svg" alt="wishlist" />
+                    </Link>
+                </div>
                 <div className="product-image">
-                    <img src="images/watch.jpg" alt="product_image" />
+                    <img src="images/watch.jpg" className='img-fluid' alt="product_image" />
+                    <img className='my-watch img-fluid' src="images/watch-1.svg" alt="product_image" />
                 </div>
                 <div className="product-details">
                     <h6 className='brand'>Havels</h6>
@@ -28,9 +35,21 @@ const ProductCard = () => {
                     <p className="price">$100.00</p>
 
                 </div>
-
+                <div className="action-bar position-absolute">
+                    <div className="d-flex flex-column gap-15">
+                        <Link>
+                            <img src="images/prodcompare.svg" alt="compare" />
+                        </Link>
+                        <Link>
+                            <img src="images/view.svg" alt="view" />
+                        </Link>
+                        <Link>
+                            <img src="images/add-cart.svg" alt="addcart" />
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
