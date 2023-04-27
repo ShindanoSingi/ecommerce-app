552f9a71de2d98860c76da3ad0082ad189cf92e3
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-04-26 22:45:45 -0400

    Added stars

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactStars from "react-rating-stars-component";
 
 const ProductCard = () => {
     return (
@@ -12,8 +13,22 @@ const ProductCard = () => {
                     <h5 className="product-title">
                         Kids headphones bulk 10 pack multi colored for students
                     </h5>
+                    <ReactStars
+                        count={5}
+                        value={3}
+                        edit={false}
+                        // onChange={ratingChanged}
+                        size={24}
+                        isHalf={true}
+                        emptyIcon={<i className="far fa-star"></i>}
+                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                        fullIcon={<i className="fa fa-star"></i>}
+                        activeColor="#ffd700"
+                    />
                     <p className="price">$100.00</p>
+
                 </div>
+
             </div>
         </div>
     )
