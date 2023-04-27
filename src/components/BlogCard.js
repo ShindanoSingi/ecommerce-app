e062be39a98208b874426a75d4d43f323d3ace02
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-04-26 22:34:17 -0400

    Added stars rating

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const BlogCard = () => {
       return (
             <div className="col-3">
-                  <div className="blog-card">ffgfgfg</div>
+                  <div className="blog-card">
+                        <div className="card-image">
+                              <img src="images/blog-1.jpg" className='img-fluid' alt="blog" />
+                        </div>
+                        <div className="blog-content">
+                              <p className="date">April 26, 2023</p>
+                              <h5 className='title'>A beautiful sunday morning renaisance</h5>
+                              <p className='desc'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur deleniti asperiores sit? Odio quasi iusto sapiente esse nihil optio voluptas, voluptatibus unde eveniet odit alias provident facere atque quam temporibus.
+                              </p>
+                              <Link to="/" className='button' >
+                                    Read More
+                              </Link>
+                        </div>
+                  </div>
             </div>
       );
 };
